9b3084de703f97aeb1f2f115addd1066948241f3
oDs <odstobiaszcod@gmail.com>
2022-04-14 22:27:19 +0200

    Added greeting and display current date

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
+
 export class NavBarComponent implements OnInit {
+
+  name:string = 'Marek';
+  currentDate:string = '';
+
+  fetchCurrentData(){
+    const today = new Date();
+    return today.getDate() + '.' + (today.getMonth()+1) + '.' + today.getFullYear() + 'r.';
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.currentDate = this.fetchCurrentData();
   }
 
 }
