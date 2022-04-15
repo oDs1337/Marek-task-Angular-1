b01fc6d74caa6573dca0d7a03810e6a0806c972e
oDs <odstobiaszcod@gmail.com>
2022-04-15 18:14:10 +0200

    Added visual space and generated ingredient component for it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    IngredientsComponent
   ],
   imports: [
     BrowserModule,
